407c2a68ff3886f547abd3c352a58d0ccd28bfc7
Sumit Basak <basaksum@umich.edu>
2018-12-25 14:35:35 -0500

    Meta changes

--- Controllers/ValuesController.cs
+++ Controllers/ValuesController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
-namespace einthu_stream.Controllers
+namespace EinthuStream.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
