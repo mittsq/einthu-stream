c4ab5218c2903b7f42689175f6a1df97e34f4619
Sumit Basak <basaksum@umich.edu>
2018-12-25 18:45:10 -0500

    Normalized links

--- Scraper.cs
+++ Scraper.cs
@@ -6,15 +6,27 @@ using AngleSharp.Dom;
 namespace EinthuStream {
     public static class Scraper {
         private const double ROUND_TO = 0.5;
+
+        private static string NormalizeLink(string link) {
+            if (link == null) { return null; }
+            if (link.StartsWith("//"))
+                return "https:" + link;
+            if (link.StartsWith("/"))
+                return "https://einthusan.tv" + link;
+            return link;
+        }
+
         public static Result Scrape(IElement parent) {
             var yearLang = parent.QuerySelector(".block2 .info > p")?.TextContent;
             var prof = parent.QuerySelectorAll(".block2 .professionals > div").Select(_ => {
                 var name = _.QuerySelector(".prof p")?.TextContent;
                 var role = _.QuerySelector(".prof label")?.TextContent;
                 var pic = _.QuerySelector(".imgwrap img")?.GetAttribute("src");
+                if (pic == "//s3.amazonaws.com/einthusanthunderbolt/etc/img/default-img.png")
+                    pic = null;
                 return new Result.Professional {
                     Name = name,
-                        Avatar = pic,
+                        Avatar = NormalizeLink(pic),
                         Role = role
                 };
             }).ToList();
@@ -33,7 +45,7 @@ namespace EinthuStream {
             var wiki = _extras?[0];
             var year = int.Parse(yearLang?.Substring(0, 4));
             return new Result {
-                CoverImageUrl = img,
+                CoverImageUrl = NormalizeLink(img),
                     Description = desc,
                     HasSubtitles = cc,
                     IsPopular = pop,
@@ -43,8 +55,8 @@ namespace EinthuStream {
                     Qualities = q,
                     Rating = rating,
                     Title = title,
-                    Trailer = trailer,
-                    Wiki = wiki,
+                    Trailer = NormalizeLink(trailer),
+                    Wiki = NormalizeLink(wiki),
                     Year = year
             };
         }
